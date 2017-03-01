f8f404ae5a19c7949abe15052a7ff54c439736ce
Ben <ben.holland.core@gmail.com>
2017-02-28 19:45:34 -0800

    commented app.js

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
+//setup the app. It'd be nice to be do this in a class.
 var express=require('express');
 var app=express();
 var http=require('http').Server(app);
 var sio=require('socket.io')(http);
-
-var users={'users':[]};
-function addUser(name){
-	users.users.push(name);
-	sio.emit('users.list',users);
-}
-function removeUser(name){
-	var index=users.users.indexOf(name);
-	if(index>-1){
-	users.users.splice(users.users.indexOf(name),1);
-	}
-	sio.emit('users.list',users);
-}
-
-
 app.set('port',(process.env.PORT||5000));
 app.use(express.static(__dirname+'/public'));
 app.set('views',__dirname+'/views');
@@ -25,6 +11,24 @@ app.get('/',function (req,res){
 		res.render('index');
 	});
 
+//Setup short-term server-side user tracking list and methods for working with it.
+//This should probably be a class, but I'm not entirely sure how to do tihs.
+var users={'users':[]};
+function addUser(name){
+	users.users.push(name);
+	sio.emit('users.list',users);
+}
+
+function removeUser(name){
+	var index=users.users.indexOf(name);
+	if(index>-1){
+	users.users.splice(users.users.indexOf(name),1);
+	}
+	sio.emit('users.list',users);
+}
+
+//Set up callbacks for socket.io and submit fake messages and users.
+//Not sure if this should go in the app class or be done independently. 
 sio.on('connection', (socket)=>{
 	socket.on('user.login.google',(data)=>{
 		console.log('User on socket '+socket['id']+'logged in.');
@@ -38,7 +42,9 @@ sio.on('connection', (socket)=>{
 	addUser(socket.id);
 	socket.emit('messages.list',{'messages':['message 1','message 2','message 3']});
 	console.log("What a wonderful connection.");
-});	
+});
+
+//start the app server.	
 http.listen(app.get('port'),function(){
 	console.log("Test listening at port "+app.get('port'));
 });
