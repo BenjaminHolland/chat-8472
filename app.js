633f081504feb3e9cfb6dfee76d7ac14f96319cf
Ben Holland <bholland@csumb.edu>
2017-03-03 18:43:18 +0000

    Got 'working' encapsulation of the app

--- app.js
+++ app.js
@@ -1,71 +1,121 @@
 'use strict';
-//setup the app. It'd be nice to be do this in a class.
-var express=require('express');
-var app=express();
-var http=require('http').Server(app);
-var sio=require('socket.io')(http);
-app.set('port',(process.env.PORT||5000));
-app.use(express.static(__dirname+'/public'));
-app.set('views',__dirname+'/views');
-app.set('view engine','ejs');
-app.get('/',function (req,res){
-		res.render('index');
-	});
-
+/*//setup the app. It'd be nice to be do this in a class.
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var sio = require('socket.io')(http);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+	res.render('index');
+});
+*/
 //Setup short-term server-side user tracking list and methods for working with it.
-//This should probably be a class, but I'm not entirely sure how to do tihs.
-class Users{
-	constructor(){
-		this._users={
-			'users':[]
+class Users {
+	constructor(sio) {
+		this._sio=sio;
+		this._users = {
+			'users': []
 		};
 	}
-	addUser(name){
+
+	addUser(name) {
 		this._users.users.push(name);
 		console.log(this._users);
-		sio.emit('users.list',this._users);	
+		this._sio.emit('users.list', this._users);
 	}
-	removeUser(name){
-		var index=this._users.users.indexOf(name);
-		if(index>-1){
-			this._users.users.splice(this._users.users.indexOf(name),1);
+
+	updateUser(currentName, newName) {
+		this._sio.emit('user.list', this._users);
+	}
+
+	removeUser(name) {
+		var index = this._users.users.indexOf(name);
+		if (index > -1) {
+			this._users.users.splice(this._users.users.indexOf(name), 1);
 		}
-		sio.emit('users.list',this._users);	
+		this._sio.emit('users.list', this._users);
 	}
 }
-var userList=new Users();
 /*
-var users={'users':[]};
-function addUser(name){
-	users.users.push(name);
-	sio.emit('users.list',users);
+var userList = new Users();
+*/
+class App {
+	constructor() {
+		this.express = require('express');
+		this.app = this.express();
+		this.http = require('http').Server(this.app);
+		this.sio = require('socket.io')(this.http);
+		this.users = new Users(this.sio);
+		console.log("Constructed");
+	}
+
+	start() {
+		console.log("starting app");
+		console.log(this.app);
+		this.app.set('port', (process.env.PORT || 5000));
+		this.app.use(this.express.static(__dirname + '/public'));
+		this.app.set('views', __dirname + '/views');
+		this.app.set('view engine', 'ejs');
+		this.app.get('/', function(req, res) {
+			res.render('index');
+		});
+		this.initCallbacks();
+		//start the app server.	
+		var lPort=this.app.get('port');
+		this.http.listen(this.app.get('port'), function() {
+			console.log("Test listening at port " + lPort);
+		});
+	}
+
+	initCallbacks() {
+		var self=this;
+		this.sio.on('connection', (socket) => {
+			socket.on('user.login.google', (data) => {
+				console.log('User on socket ' + socket['id'] + 'logged in.');
+				console.log(JSON.stringify(data, null, 2));
+			});
+			socket.on('disconnect', (data) => {
+				console.log('User disconnected.');
+				console.log(self);
+				self.users.removeUser(socket.id);
+			});
+			self.users.addUser(socket.id);
+			socket.emit('messages.list', {
+				'messages': ['message 1', 'message 2', 'message 3']
+			});
+			console.log("What a wonderful connection.");
+		});
+	}
+
 }
 
-function removeUser(name){
-	var index=users.users.indexOf(name);
-	if(index>-1){
-	users.users.splice(users.users.indexOf(name),1);
-	}
-	sio.emit('users.list',users);
-}
-*/
+var server=new App();
+server.start();
+
+/*
 //Set up callbacks for socket.io and submit fake messages and users.
 //Not sure if this should go in the app class or be done independently. 
-sio.on('connection', (socket)=>{
-	socket.on('user.login.google',(data)=>{
-		console.log('User on socket '+socket['id']+'logged in.');
-		console.log(JSON.stringify(data,null,2));
+sio.on('connection', (socket) => {
+	socket.on('user.login.google', (data) => {
+		console.log('User on socket ' + socket['id'] + 'logged in.');
+		console.log(JSON.stringify(data, null, 2));
 	});
-	socket.on('disconnect',(data)=>{
+	socket.on('disconnect', (data) => {
 		console.log('User disconnected.');
 		userList.removeUser(socket.id);
 	});
 	userList.addUser(socket.id);
-	socket.emit('messages.list',{'messages':['message 1','message 2','message 3']});
+	socket.emit('messages.list', {
+		'messages': ['message 1', 'message 2', 'message 3']
+	});
 	console.log("What a wonderful connection.");
 });
 
 //start the app server.	
-http.listen(app.get('port'),function(){
-	console.log("Test listening at port "+app.get('port'));
+http.listen(app.get('port'), function() {
+	console.log("Test listening at port " + app.get('port'));
 });
+*/
